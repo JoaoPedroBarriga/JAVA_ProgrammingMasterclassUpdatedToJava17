ddc1f9326c0860b3439ad9be791669097d769fbb
João Pedro Barriga <jotapbarriga@gmail.com>
2023-03-13 14:27:39 +0000

    "Section04_IntelliJ_Basics"
    This section goes from Classes_032_Through_039.
    
    Getting to know packages, resources, how to properly organize the structures.

--- !Section04_IntellijBasics/src/Classes_032_Through_039/_032_HelloWorld_FirstClass.java
+++
@@ -1,8 +0,0 @@
-package Classes_032_Through_039;
-
-public class _032_HelloWorld_FirstClass {
-    public static void main(String[] args) {
-        //Difference, print <- -> NOT println
-        System.out.print("Hello, sDevPT");
-    }
-}
--- !Section04_IntellijBasics/src/Classes_032_Through_039/_033_HelloWorld_SecondClass_NotepadPaste.java
+++
@@ -1,8 +0,0 @@
-package Classes_032_Through_039;
-
-public class _033_HelloWorld_SecondClass_NotepadPaste {
-    public static void main(String[] args) {
-        //Difference, print <- -> NOT println
-        System.out.print("Hello, sDevPT");
-    }
-}
